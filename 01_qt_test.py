a8ff72fb1c2ad98a95227740884336c04baaaca4
Elfen20 <elfen20@outlook.de>
2024-08-07 09:58:31 +0200

    + pyside6 qt test file, shows version

---
+++ 01_qt_test.py
@@ -0,0 +1,8 @@
+# 01 QT Test
+import PySide6.QtCore
+
+# Prints PySide6 version
+print(f"PySide 6 Version: {PySide6.__version__}")
+
+# Prints the Qt version used to compile PySide6
+print(f"Pyside 6 QtCode Version: {PySide6.QtCore.__version__}")
